f8ee505fedc2d3a03439de6585957b069140d46d
Matteo Beniamino <beniamino@tautologica.org>
2015-01-18 10:07:45 +0100

    Rimossi tab

--- missioni.py
+++ missioni.py
@@ -7,13 +7,13 @@ import sys
 print "Content-Type: text/html"
 print
 try:
-	pwd = os.path.dirname(os.path.abspath(__file__))
-	proc = subprocess.Popen(["%s/missioni-data/polygen" % pwd, "%s/missioni-data/grammar" % pwd], stdout=subprocess.PIPE)
-	(out, err) = proc.communicate()
-	titolo, descrizione, _ = out.splitlines()
+    pwd = os.path.dirname(os.path.abspath(__file__))
+    proc = subprocess.Popen(["%s/missioni-data/polygen" % pwd, "%s/missioni-data/grammar" % pwd], stdout=subprocess.PIPE)
+    (out, err) = proc.communicate()
+    titolo, descrizione, _ = out.splitlines()
 except Exception, e:
-	print e
-	sys.exit(0)
+    print e
+    sys.exit(0)
 
 print """
 <!DOCTYPE html>
@@ -27,14 +27,14 @@ print """
         .titolo {color: #00ffff;font-size: 200%%;}
         .autore {color: #00ff00;font-size: 200%%;}
         .descrizione {color: #007777;font-size: 200%%;}
-	.bottom { position: absolute; bottom: 0;}
+    .bottom { position: absolute; bottom: 0;}
         </style>
     </head>
     <body>
         <div class="titolo"><h1>%s</h1></div>
         <div class="autore"><p>NoTAVernello<p></div>
         <div class="descrizione"><p>%s</p></div>
-	<div class="bottom"><a href="javascript:location.reload()">Un altro!</a></div>
+    <div class="bottom"><a href="javascript:location.reload()">Un altro!</a></div>
     </body>
 </html>
 """ % (titolo,descrizione)
